aa202b686b93e9ac78f237ab4e4427bf04f33260
garycalvin <garycalvin@constitution.hsd1.va.comcast.net>
2021-05-05 09:01:03 -0400

    Added funcs and tests

--- chess.py
+++ chess.py
@@ -3,9 +3,210 @@ Created on May 5, 2021
 
 @author: garycalvin
 '''
+COLUMNS = 'abcdefgh'
+ROWS = '12345678'
+PIECES = {'P': 1, 'R': 2, 'N': 3, 'B': 4, 'Q': 5, 'K': 6}
+STARTING_POS = [2, 3, 4, 5, 6, 4, 3, 2,
+                1, 1, 1, 1, 1, 1, 1, 1,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                -1, -1, -1, -1, -1, -1, -1, -1,
+                -2, -3, -4, -5, -6, -4, -3, -2]
 
 
 def column_num(col):
     assert len(col) == 1
     assert col >= 'a' and col <= 'h'
     return ord(col) - ord('a')
+
+
+def row_num(row):
+    assert len(row) == 1
+    assert row >= '1' and row <= '8'
+    return ord(row) - ord('1')
+
+
+def algebraic_to_index(alg):
+    assert len(alg) == 2
+    return row_num(alg[1]) * 8 + column_num(alg[0])
+
+
+def index_to_algebraic(num):
+    assert int == type(num)
+    assert num >= 0 and num <= 63
+    col = COLUMNS[num % 8]
+    row = ROWS[num // 8]
+    return col + row
+
+
+def piece_alpha(num):
+    num = abs(num)
+    assert num in PIECES.values()
+    for key, value in PIECES.items():
+        if value == num:
+            result = key
+            break
+    return result
+
+
+def piece_num(alpha, color='white'):
+    assert len(alpha) == 1
+    assert alpha in PIECES.keys()
+    sign = 1 if color == 'white' else -1
+    return sign * PIECES[alpha]
+
+
+def sign(num):
+    return 0 if num == 0 else -1 if num < 0 else 1
+
+
+def rank_distance(src, dest):
+    if str == type(src):
+        src = algebraic_to_index(src)
+    if str == type(dest):
+        dest = algebraic_to_index(dest)
+
+    assert src >= 0 and src <= 63
+    assert dest >= 0 and dest <= 63
+
+    return abs(dest // 8 - src // 8)
+
+
+def file_distance(src, dest):
+    if str == type(src):
+        src = algebraic_to_index(src)
+    if str == type(dest):
+        dest = algebraic_to_index(dest)
+
+    assert src >= 0 and src <= 63
+    assert dest >= 0 and dest <= 63
+
+    return abs(dest % 8 - src % 8)
+
+
+def locate_pieces(position, piece, color='white'):
+    if str == type(piece):
+        piece = piece_num(piece)
+    sign = 1 if color == 'white' else -1
+    piece = sign * piece
+    result = []
+    for i in range(len(position)):
+        if position[i] == piece:
+            result.append(i)
+
+    return result
+
+
+def get_direction(src, dest):
+    if str == type(src):
+        src = algebraic_to_index(src)
+    if str == type(dest):
+        dest = algebraic_to_index(dest)
+    assert src >= 0 and src <= 63
+    assert dest >= 0 and dest <= 63
+    assert src != dest
+
+    return sign(dest - src) * 8 + sign(dest % 8 - src % 8)
+
+
+def pawn_move_can_reach(position, src, dest):
+    if str == type(src):
+        src = algebraic_to_index(src)
+    if str == type(dest):
+        dest = algebraic_to_index(dest)
+    assert src >= 0 and src <= 63
+    assert dest >= 0 and dest <= 63
+    assert src != dest
+    my_sign = sign(position[src])
+
+    if dest - src == my_sign * 8:
+        if position[dest] == 0:
+            return True
+
+    if dest - src == my_sign * 16 and position[dest] == 0:
+        # make sure we're on a starting square
+        if position[src] == STARTING_POS[src]:
+            if position[src + my_sign * 8] == 0:
+                return True
+
+    if dest - src == my_sign * 7 \
+            and dest % 8 - src % 8 == dest // 8 - src // 8 \
+            and sign(position[dest]) != my_sign:
+        return True
+
+    if dest - src == my_sign * 9 \
+            and dest % 8 - src % 8 == dest // 8 - src // 8 \
+            and sign(position[dest]) != my_sign:
+        return True
+
+    return False
+
+
+def rook_move_can_reach(position, src, dest):
+    if str == type(src):
+        src = algebraic_to_index(src)
+    if str == type(dest):
+        dest = algebraic_to_index(dest)
+    assert src >= 0 and src <= 63
+    assert dest >= 0 and dest <= 63
+    assert src != dest
+
+    if 0 == rank_distance(src, dest):
+        d = -1 if dest < src else 1
+        nxt = src + d
+        while nxt != dest:
+            if position[nxt] != 0:
+                return False
+            nxt += d
+        return True
+
+    if 0 == file_distance(src, dest):
+        d = -8 if dest < src else 8
+        nxt = src + d
+        while nxt != dest:
+            if position[nxt] != 0:
+                return False
+            nxt += d
+        return True
+    return False
+
+
+def knight_move_can_reach(position, src, dest):
+    if str == type(src):
+        src = algebraic_to_index(src)
+    if str == type(dest):
+        dest = algebraic_to_index(dest)
+    assert src >= 0 and src <= 63
+    assert dest >= 0 and dest <= 63
+    assert src != dest
+
+    if abs(dest - src) in [6, 10, 15, 17]:
+        if rank_distance(src, dest) + file_distance(src, dest) == 3:
+            if sign(position[dest]) != sign(position[src]):
+                return True
+
+    return False
+
+
+def bishop_move_can_reach(position, src, dest):
+    if str == type(src):
+        src = algebraic_to_index(src)
+    if str == type(dest):
+        dest = algebraic_to_index(dest)
+    assert src >= 0 and src <= 63
+    assert dest >= 0 and dest <= 63
+    assert src != dest
+
+    if rank_distance(src, dest) == file_distance(src, dest):
+        if sign(position[src]) != sign(position[dest]):
+            d = get_direction(src, dest)
+            nxt = src + d
+            while nxt != dest:
+                if position[nxt] != 0:
+                    return False
+                nxt += d
+            return True
+
+    return False
--- test_chess.py
+++ test_chess.py
@@ -6,8 +6,45 @@ Created on May 5, 2021
 import unittest
 import chess as ch
 
+STARTING_POS = [2, 3, 4, 5, 6, 4, 3, 2,
+                1, 1, 1, 1, 1, 1, 1, 1,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                -1, -1, -1, -1, -1, -1, -1, -1,
+                -2, -3, -4, -5, -6, -4, -3, -2]
 
-class TestChess(unittest.TestCase):
+
+POS1 = [0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0]
+
+POS2 = [0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 1, 0, 0, 2, 6, 0, 1,
+        1, 0, 0, -2, 0, 1, 0, -1,
+        0, 0, 0, 0, 0, -1, 1, 0,
+        0, -1, 0, 0, 1, 0, -6, 0,
+        -1, 0, 0, 0, 0, -1, -1, 0,
+        0, 0, 0, 0, 0, 0, 0, 0]
+
+POS3 = [2, 0, 0, 0, 0, 2, 6, 0,
+        1, 4, 1, 0, 0, 3, 1, 1,
+        -1, 0, 0, 1, 0, 5, 0, 3,
+        0, -1, 0, 0, 0, 1, 0, 0,
+        0, 0, 0, 0, 0, -1, 0, 4,
+        0, 0, -3, 0, -1, 0, 0, -1,
+        0, -1, 0, -3, -4, 0, -1, 0,
+        -2, 0, -4, -5, 0, -2, -6, 0]
+
+
+class Test(unittest.TestCase):
 
     def test_column_num(self):
         result = ch.column_num('a')
@@ -15,7 +52,114 @@ class TestChess(unittest.TestCase):
         result = ch.column_num('h')
         assert result == 7
 
+    def test_row_num(self):
+        result = ch.row_num('1')
+        assert result == 0
+        result = ch.row_num('8')
+        assert result == 7
+
+    def test_algebraic_to_index(self):
+        result = ch.algebraic_to_index('a1')
+        assert result == 0
+        result = ch.algebraic_to_index('h8')
+        assert result == 63
+
+    def test_index_to_algebraic(self):
+        result = ch.index_to_algebraic(0)
+        assert result == 'a1'
+
+    def test_piece_alpha(self):
+        result = ch.piece_alpha(1)
+        assert result == 'P'
+        result = ch.piece_alpha(6)
+        assert result == 'K'
+        result = ch.piece_alpha(-5)
+        assert result == 'Q'
+
+    def test_piece_num(self):
+        result = ch.piece_num('Q')
+        assert result == 5
+        result = ch.piece_num('B', 'black')
+        assert result == -4
+
+    def test_sign(self):
+        result = ch.sign(-123)
+        assert result == -1
+        result = ch.sign(0)
+        assert result == 0
+        result = ch.sign(12345)
+        assert result == 1
+
+    def test_rank_distance(self):
+        result = ch.rank_distance(0, 63)
+        assert result == 7
+        result = ch.rank_distance('a3', 'd5')
+        assert result == 2
+
+    def test_file_distance(self):
+        result = ch.file_distance(0, 63)
+        assert result == 7
+        result = ch.file_distance('a3', 'd5')
+        assert result == 3
+
+    def test_locate_pieces(self):
+        result = ch.locate_pieces(POS1, 1)
+        assert result == []
+        result = ch.locate_pieces(POS2, 1)
+        assert result == [17, 23, 24, 29, 38, 44]
+        result = ch.locate_pieces(POS2, 'K')
+        assert result == [21]
+        result = ch.locate_pieces(POS2, 'K', 'black')
+        assert result == [46]
+        result = ch.locate_pieces(POS3, -3)
+        assert result == [42, 51]
+
+    def test_get_direction(self):
+        result = ch.get_direction('c8', 'a6')
+        assert result == -9
+        result = ch.get_direction('b2', 'a3')
+        assert result == 7
+
+    def test_pawn_move_can_reach(self):
+        result = ch.pawn_move_can_reach(STARTING_POS, 11, 19)
+        assert result is True
+        result = ch.pawn_move_can_reach(STARTING_POS, 'd2', 'd4')
+        assert result is True
+        result = ch.pawn_move_can_reach(STARTING_POS, 'd2', 'd5')
+        assert result is False
+        result = ch.pawn_move_can_reach(POS2, 'f5', 'f4')
+        assert result is False
+        result = ch.pawn_move_can_reach(POS2, 'f7', 'f6')
+        assert result is True
+        result = ch.pawn_move_can_reach(POS2, 'e6', 'f7')
+        assert result is True
+
+    def test_rook_move_can_reach(self):
+        result = ch.rook_move_can_reach(POS2, 'd4', 'a4')
+        assert result is True
+        result = ch.rook_move_can_reach(POS2, 'd4', 'e3')
+        assert result is False
+
+    def test_knight_move_can_reach(self):
+        result = ch.knight_move_can_reach(POS3, 'f2', 'e4')
+        assert result is True
+        result = ch.knight_move_can_reach(POS3, 'f2', 'd4')
+        assert result is False
+        result = ch.knight_move_can_reach(POS3, 23, 33)
+        assert result is False
+        result = ch.knight_move_can_reach(POS3, 23, 38)
+        assert result is True
+
+    def test_bishop_move_can_reach(self):
+        result = ch.bishop_move_can_reach(POS3, 'c8', 'a6')
+        assert result is False
+        result = ch.bishop_move_can_reach(POS3, 'b2', 'g7')
+        assert result is True
+        result = ch.bishop_move_can_reach(POS3, 'b2', 'a3')
+        assert result is True
+        result = ch.bishop_move_can_reach(POS3, 47, 56)
+        assert result is False
+
 
 if __name__ == "__main__":
-    #import sys;sys.argv = ['', 'Test.test']
     unittest.main()
